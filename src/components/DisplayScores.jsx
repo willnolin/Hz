dc494b307960abfb39dcd22c083ba92881633873
Will Metivier <willnolin@gmail.com>
2021-06-08 20:24:48 -0400

    high score order

--- src/components/DisplayScores.jsx
+++ src/components/DisplayScores.jsx
@@ -4,30 +4,30 @@ import { getAllHighScores } from '../services/api.js'
 
 export default function DisplayScores() {
   const [scores, setScores] = useState([])
-
+ // get high scores from API
   useEffect(()=> {
     const fetchData = async () => {
-      const res = await getAllHighScores();
-      
+      const res = await getAllHighScores();  
       const data = res.records;
-      console.log(data)
-      setScores(data);
+      // sort Scores in descending order and set state
+      setScores(sortScores(data));
     } 
    fetchData()
   }, []);
 
   const sortScores = (array)=> {
-    
+    return (array.sort((a,b) =>  b.fields.score-a.fields.score ))
   }
 
   return (
     <>
     <div className="scores-list"> 
+      
       {scores.map((entry, index) => {
         return <div className = "score-entry" key={index}>
-          <h4>{entry.fields.user}</h4>
-          <h5>{`${entry.fields.score} pts`}</h5>
-        <h5>{entry.createdTime.slice(0,10)}</h5> </div>
+          <h4 className= "scores-field">{entry.fields.user}</h4>
+          <h5 className= "scores-field">{`${entry.fields.score} pts`}</h5>
+        <h5 className= "scores-field">{entry.createdTime.slice(0,10)}</h5> </div>
       })}
     </div>
     </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
       <Link className="scores-link" to="/scores">
         <h3>high scores</h3>
       </Link>
-      <h1>Hz</h1>
+      <h1 className="app-title">Hz</h1>
       <Link className="scores-link" to="/quiz">
         <h3>start quiz</h3>
       </Link>
