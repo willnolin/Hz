9747806e305b2b13abf14673731484cb575e6745
Will Metivier <willnolin@gmail.com>
2021-06-11 21:00:15 -0400

    mobile style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import gitIcon from '../images/github-icon.png';
+import linkedIcon from '../images/linkedin-icon.png';
 function Footer() {
   return (
     <footer className="footer-container">
-      This is the Footer
+      <a href="https://github.com/willnolin/Hz"><img style={{ width: '32px' }} src={gitIcon} alt="github" /></a>
+      <p> @ Will Metivier</p>
+      <a href="https://www.linkedin.com/in/will-metivier-1ab10541/"><img style={{width: '32px'}} src={linkedIcon} alt="linkedin" /></a>
     </footer>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,12 +7,12 @@ export default function Header() {
       <img src={screw} alt="screw" className="screw"></img>
      
       <div className = "nav-bar" >
-        <Link to="/scores">
-          <h3 className= "scores-link">high scores</h3>
-        </Link> 
-        <Link to="/quiz">
-          <h3 className="scores-link" >start quiz</h3>
+        <Link className="nav-link" to="/quiz">
+          <h3>START</h3>
         </Link>
+        <Link className= "nav-link" to="/scores">
+          <h3 >HIGH SCORES</h3>
+        </Link> 
      
       </div>
       <div className = "app-title">  
